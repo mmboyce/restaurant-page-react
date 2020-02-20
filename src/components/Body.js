da5de9acd0ce7a3334cf8e036431dd5ff88b41f2
mmboyce <mathieumimmsboyce@gmail.com>
2020-02-20 02:25:16 -0500

    Add handlers to Body

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles/Body.css';
-import Sitemap from './Sitemap';
+import './styles/Sitemap.css';
 import Menu from './Menu';
 import About from './About';
 
@@ -10,13 +10,62 @@ class Body extends React.Component {
     this.state = {
       ref: 'sitemap',
     };
+
+    this.handleSelection = this.handleSelection.bind(this);
+    this.handleMenu = this.handleMenu.bind(this);
+    this.handleAbout = this.handleAbout.bind(this);
+  }
+
+  handleSelection(newRef) {
+    this.setState({
+      ref: newRef,
+    });
+  }
+
+  handleMenu() {
+    this.handleSelection('menu');
+  }
+
+  handleAbout() {
+    this.handleSelection('about');
   }
 
   render() {
     const { ref } = this.state;
     let display;
     if (ref === 'sitemap') {
-      display = <Sitemap />;
+      display = (
+        <div className="sitemap">
+          <div
+            id="aboutRef"
+            className="ref"
+            onClick={this.handleAbout}
+            onKeyDown={(e) => {
+              if (e.keyCode === 13) {
+                this.handleAbout();
+              }
+            }}
+            role="button"
+            tabIndex="0"
+          >
+            About
+          </div>
+          <div
+            id="menuRef"
+            className="ref"
+            onClick={this.handleMenu}
+            onKeyDown={(e) => {
+              if (e.keyCode === 13) {
+                this.handleMenu();
+              }
+            }}
+            role="button"
+            tabIndex="0"
+          >
+            Menu
+          </div>
+        </div>
+      );
     } else if (ref === 'menu') {
       display = <Menu />;
     } else if (ref === 'about') {
