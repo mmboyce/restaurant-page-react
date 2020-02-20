b734b69f783ea20700f1630727df415da4262b95
mmboyce <mathieumimmsboyce@gmail.com>
2020-02-20 04:52:05 -0500

    Style site to match original.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Body from './components/Body';
+import Credit from './components/Credit';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Body />
+      <Credit />
     </div>
   );
 }
---
+++ src/components/Credit.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles/Credit.css';
+
+function Credit() {
+  return (
+    <div id="footer">
+      <div id="credit">
+        Created by
+        {' '}
+        <a href="http://mmboyce.github.io/">W Mathieu Mimms-Boyce</a>
+      </div>
+    </div>
+  );
+}
+
+export default Credit;
