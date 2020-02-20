aeaa2b5a243010e561a01e5807e2c0710dcc0d7f
mmboyce <mathieumimmsboyce@gmail.com>
2020-02-20 05:00:18 -0500

    Updates

---
+++ precache-manifest.6ce5a70aca1088a51620ca96bf426295.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92b4edaea2ce9f4f42ddefaa09c8001e",
+    "url": "/restaurant-page-react/index.html"
+  },
+  {
+    "revision": "6779e12b886ee40372eb",
+    "url": "/restaurant-page-react/static/css/main.9bbcb51f.chunk.css"
+  },
+  {
+    "revision": "c7e03615209d99bdb89c",
+    "url": "/restaurant-page-react/static/js/2.7d30fa8d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/restaurant-page-react/static/js/2.7d30fa8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6779e12b886ee40372eb",
+    "url": "/restaurant-page-react/static/js/main.8689333a.chunk.js"
+  },
+  {
+    "revision": "a1f476d2221df92b50b2",
+    "url": "/restaurant-page-react/static/js/runtime-main.df82d1a3.js"
+  },
+  {
+    "revision": "fcecf5072bb1e8aa2773ee0ccf8a3045",
+    "url": "/restaurant-page-react/static/media/header.fcecf507.jpeg"
+  }
+]);
